66fdbd13870b628787ad1e7668c39e372c4c0d81
juykyung.kim <jukyung.kim@nhn.com>
2018-07-27 16:22:15 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super();
+	}
+  render() {
+    return (
+      <div className="App">
+        실행했다
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Star.jsx
@@ -0,0 +1,30 @@
+/*
+*
+* Star
+*
+*/
+// import React, {Component, Fragment} from "react";
+import PropTypes from "prop-types";
+
+
+export class Star extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+				스타
+			</div>
+		);
+	}
+}
+
+Star.defaultProps = {};
+
+Star.propTypes = {};
+
+export default Star;
